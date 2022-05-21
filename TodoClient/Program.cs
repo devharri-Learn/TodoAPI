6103627342f94332d7af2239c2b0e16085cf096f
Devharri <devharri@outlook.com>
2022-05-21 12:21:33 +0300

    formatting program cs

--- TodoClient/Program.cs
+++ TodoClient/Program.cs
@@ -1,44 +1,40 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.Net.Http.Headers;
 using TodoClient.Models;
 
 
-        using (var client = new HttpClient())
+using (var client = new HttpClient())
+{
+
+    client.BaseAddress = new Uri("https://localhost:7283");
+    client.DefaultRequestHeaders.Accept.Clear();
+    client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+
+    //HTTP GET
+    var responseTask = client.GetAsync("api/TodoItems");
+    responseTask.Wait();
+
+    var result = responseTask.Result;
+    if (result.IsSuccessStatusCode)
+    {
+
+        var readTask = result.Content.ReadAsAsync<TodoItemDTO[]>();
+        readTask.Wait();
+
+        var students = readTask.Result;
+
+        foreach (var student in students)
         {
-
-            client.BaseAddress = new Uri("https://localhost:7283");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //HTTP GET
-            var responseTask = client.GetAsync("api/TodoItems");
-            responseTask.Wait();
-
-            var result = responseTask.Result;
-            if (result.IsSuccessStatusCode)
-            {
-
-                var readTask = result.Content.ReadAsAsync<TodoItemDTO[]>();
-                readTask.Wait();
-
-                var students = readTask.Result;
-
-                foreach (var student in students)
-                {
-                    Console.WriteLine(student.Name);
-                }
-            }
-            else
-            {
-                Console.WriteLine("oops");
-            }
+            Console.WriteLine(student.Name);
         }
+    }
+    else
+    {
+        Console.WriteLine("oops");
+    }
+}
 Console.WriteLine("Press any key to POST");
-        Console.ReadLine();
+Console.ReadLine();
 using (var client = new HttpClient())
 {
 
@@ -49,7 +45,7 @@ using (var client = new HttpClient())
 
     //HTTP POST
 
-    TodoItemDTO todoItemDTO = new TodoItemDTO() { Name = "CONSOLEPOSTTEST", IsComplete = false};
+    TodoItemDTO todoItemDTO = new TodoItemDTO() { Name = "CONSOLEPOSTTEST", IsComplete = false };
 
     var postTask = client.PostAsJsonAsync<TodoItemDTO>("api/TodoItems", todoItemDTO);
     postTask.Wait();
